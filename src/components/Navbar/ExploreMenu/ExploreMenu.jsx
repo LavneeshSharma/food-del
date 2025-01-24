2ec10ea953a6cc38860ad84fe2bbf207cee7e2f8
LavneeshSharma <2023itb028.lavneesh@students.iiests.ac.in>
2025-01-24 12:27:40 +0530

    Home updates

--- src/components/Navbar/ExploreMenu/ExploreMenu.jsx
+++ src/components/Navbar/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ExploreMenu.css'
 import {menu_list} from '../../../assets/frontend_assets/assets.js'
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
        <h1>Explore our menu</h1>
@@ -9,14 +9,15 @@ const ExploreMenu = () => {
        <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
             return(
-               <div className="explore-menu-list-item">
-                   <img src={item.menu_image} alt=""/>
+               <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} className="explore-menu-list-item">
+                   <img className={category===item.menu_name?"active":""}src={item.menu_image} alt=""/>
                    <p>{item.menu_name}</p>
                </div>
             )
         })
         }
        </div>
+       <hr />
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../../components/Navbar/Header/Header'
 import './Home.css'
+import { useState } from 'react'
+import ExploreMenu from '../../components/Navbar/ExploreMenu/ExploreMenu'
 const Home = () => {
+  const[category,setCategory]=useState("All");
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
