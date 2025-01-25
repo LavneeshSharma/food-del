173357275b30dac7ca62556ed4f12b2c9e882ba9
LavneeshSharma <2023itb028.lavneesh@students.iiests.ac.in>
2025-01-25 17:42:19 +0530

    Responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import pic from "../../assets/admin_assets/logo2.png";
 import pic2 from "../../assets/frontend_assets/search_icon.png";
 import pic3 from "../../assets/frontend_assets/basket_icon.png";
+import {Link} from 'react-router-dom'
 import "./Navbar.css"
 const Navbar = () => {
     const [menu,setMenu]=useState("home");
@@ -9,10 +10,10 @@ const Navbar = () => {
     <div className='navbar'>
        <img src={pic} alt="" className='logo'/>
        <ul className="navbar-menu">
-         <li onClick={()=>setMenu("home")} className={menu==='home'?"active":""}>home</li>
-         <li onClick={()=>setMenu("menu")} className={menu==='menu'?"active":""}>menu</li>
-         <li onClick={()=>setMenu("mobile-app")} className={menu==='mobile-app'?"active":""}>mobile-app</li>
-         <li onClick={()=>setMenu("contact-us")} className={menu==='contact-us'?"active":""}>contact us</li>
+         <Link to='/' onClick={()=>setMenu("home")} className={menu==='home'?"active":""}>home</Link>
+         <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==='menu'?"active":""}>menu</a>
+         <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==='mobile-app'?"active":""}>mobile-app</a>
+         <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==='contact-us'?"active":""}>contact us</a>
        </ul>
        <div className="navbar-right">
           <img src={pic2} alt=""/>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import './Home.css'
 import { useState } from 'react'
 import ExploreMenu from '../../components/Navbar/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/Navbar/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/Navbar/AppDownload/AppDownload'
 const Home = () => {
   const[category,setCategory]=useState("All");
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
