230e2346b0539667d0a980a2534f73cecc3f923b
LavneeshSharma <2023itb028.lavneesh@students.iiests.ac.in>
2025-02-05 23:46:17 +0530

    order cart updates

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,19 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 const app=express()
 const port=3000
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-connectdb();
-
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
+connectdb();
 app.get('/',(req,res)=>{
     res.send("API working")
 })
--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -40,11 +40,16 @@ const StoreContextProvider=(props)=>{
         const response=await axios.get(url+"/api/food/list");
         setFoodlist(response.data.data)
     }
+    const loadCartData=async(token)=>{
+        const response=await axios.post(url+"/api/cart/get",{},{headers:{token}});
+        setCartItems(response.data.cartData);
+    }
     useEffect(()=>{
         async function loadData() {
             await fetchFoodList();
             if(localStorage.getItem("token")){
             setToken(localStorage.getItem("token"));
+            await loadCartData(localStorage.getItem("token"));
         }
         }
         loadData();
