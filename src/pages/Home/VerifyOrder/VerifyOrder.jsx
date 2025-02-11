b723a9354dc25092dbe66b73bf9c96f20c36f7fa
LavneeshSharma <2023itb028.lavneesh@students.iiests.ac.in>
2025-02-11 23:57:12 +0530

    add loading spinner and implement payment verification logic

--- src/pages/Home/VerifyOrder/VerifyOrder.jsx
+++ src/pages/Home/VerifyOrder/VerifyOrder.jsx
@@ -1,23 +1,37 @@
-import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
-
+import { useContext, useEffect } from "react";
+import { useSearchParams,useNavigate } from "react-router-dom";
+import axios from "axios";
+import { StoreContext } from '../../../Context/StoreContext'
 const VerifyOrder = () => {
   const [searchParams] = useSearchParams();
   const success = searchParams.get("success");
   const orderId = searchParams.get("orderId");
-  
+  const {url}=useContext(StoreContext);
+
   useEffect(() => {
     if (success === "true") {
       console.log(`Payment Successful for Order ID: ${orderId}`);
     } else {
       console.log("Payment Failed");
     }
+    verifyPayment();
   }, [success, orderId]);
+  const navigate=useNavigate();
+  const verifyPayment=async()=>{
+    const response=await axios.post(url+"/api/order/verify",{success,orderId});
+    if(response.data.success)
+    {
+       navigate("/myorders");
+    }
+    else
+    {
+      navigate("/");
+    }
+  }
 
   return (
-    <div>
-      <h2>{success === "true" ? "✅ Payment Successful!" : "❌ Payment Failed"}</h2>
-      <p>Order ID: {orderId}</p>
+    <div className="verify">
+      <div className="spinner"></div>
     </div>
   );
 };
